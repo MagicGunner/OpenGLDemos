db9a5b21804ffe3c7573fa92b32d871b597e0aac
MissBlue <magicguner124@gmail.com>
2024-09-14 17:32:40 +0800

    作业2

--- Demo2/main.cpp
+++ Demo2/main.cpp
@@ -1,41 +1,35 @@
-#include "Triangle.h"
-#include "rasterizer.h"
-#include <Eigen/Eigen>
+// clang-format off
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "rasterizer.h"
+#include "global.h"
+#include "Triangle.h"
 
 constexpr double MY_PI = 3.1415926;
 
-Matrix4f get_view_matrix(Vector3f eye_pos) {
-    Matrix4f view = Matrix4f::Identity();
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
-    Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0],
-            0, 1, 0, -eye_pos[1],
-            0, 0, 1, -eye_pos[2],
-            0, 0, 0, 1;
+    Eigen::Matrix4f translate;
+    translate << 1,0,0,-eye_pos[0],
+                 0,1,0,-eye_pos[1],
+                 0,0,1,-eye_pos[2],
+                 0,0,0,1;
 
-    view = translate * view;
+    view = translate*view;
 
     return view;
 }
 
-Matrix4f get_model_matrix(float rotation_angle) {
-    Matrix4f model = Matrix4f::Identity();
-
-    // TODO: Implement this function
-    // Create the model matrix for rotating the triangle around the Z axis.
-    // Then return it.
-    float rad = rotation_angle * MY_PI / 180;
-    model(0, 0) = cos(rad);
-    model(1, 1) = cos(rad);
-    model(0, 1) = -sin(rad);
-    model(1, 0) = sin(rad);
+Eigen::Matrix4f get_model_matrix(float rotation_angle)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
     return model;
 }
 
-Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
-                               float zNear, float zFar) {
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar)
+{
     // Students will implement this function
 
     Matrix4f projection = Matrix4f::Identity();
@@ -62,72 +56,93 @@ Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     return projection;
 }
 
-int main(int argc, const char **argv) {
+int main(int argc, const char** argv)
+{
     float angle = 0;
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
+    if (argc == 2)
+    {
         command_line = true;
-        angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
-            filename = std::string(argv[3]);
-        } else
-            return 0;
+        filename = std::string(argv[1]);
     }
 
     rst::rasterizer r(700, 700);
 
-    Vector3f eye_pos = {0, 0, 5};
+    Eigen::Vector3f eye_pos = {0,0,5};
 
-    std::vector<Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
 
-    std::vector<Vector3i> ind{{0, 1, 2}};
+    std::vector<Eigen::Vector3f> pos
+            {
+                    {2, 0, -2},
+                    {0, 2, -2},
+                    {-2, 0, -2},
+                    {3.5, -1, -5},
+                    {2.5, 1.5, -5},
+                    {-1, 0.5, -5}
+            };
+
+    std::vector<Eigen::Vector3i> ind
+            {
+                    {0, 1, 2},
+                    {3, 4, 5}
+            };
+
+    std::vector<Eigen::Vector3f> cols
+            {
+                    {217.0, 238.0, 185.0},
+                    {217.0, 238.0, 185.0},
+                    {217.0, 238.0, 185.0},
+                    {185.0, 217.0, 238.0},
+                    {185.0, 217.0, 238.0},
+                    {185.0, 217.0, 238.0}
+            };
 
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
+    auto col_id = r.load_colors(cols);
 
     int key = 0;
     int frame_count = 0;
 
-    if (command_line) {
+    if (command_line)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
 
         cv::imwrite(filename, image);
 
         return 0;
     }
 
-    while (key != 27) {
+    while(key != 27)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
 
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
         cv::imshow("image", image);
         key = cv::waitKey(10);
 
         std::cout << "frame count: " << frame_count++ << '\n';
-
-        if (key == 'a') {
-            angle += 10;
-        } else if (key == 'd') {
-            angle -= 10;
-        }
     }
 
     return 0;
 }
+// clang-format on
